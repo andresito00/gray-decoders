d58f9a09a7e9a587f78e783e335127a7bff18f11
andresito00 <andres.nandez.00@gmail.com>
2020-09-10 22:40:54 -0400

    Some more project setup. Trying out TCLAP.

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++
+# export CC=/usr/bin/clang
+# export CXX=/usr/bin/clang++
 
 mkdir -p build
 
 cmake\
+ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON\
  -D_CMAKE_TOOLCHAIN_PREFIX=llvm-\
- -DCMAKE_USER_MAKE_RULES_OVERRIDE=ClangOverrides.txt
+ -Bbuild\
+
+make -C ./build
+
+cp ./build/cpp/decode .
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Download 3rd party source
+git clone --depth 1 --branch v1.2.2 https://github.com/mirror/tclap.git ./cpp/lib/tclap/
