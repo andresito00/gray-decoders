70ac5a705c1022cd45d1ddfef8d64e4018524b8e
andresito00 <andres.nandez.00@gmail.com>
2023-08-26 14:06:22 -0400

    Wrap ConcurrentQueue with a condition variable to notify consumers when to wake up.

--- cpp/decode.cpp
+++ cpp/decode.cpp
@@ -2,6 +2,8 @@
 #include <csignal>
 #include <chrono>
 #include <memory>
+#include <mutex>
+#include <condition_variable>
 #include <unistd.h>
 #include <safer_thread.h>
 #include <raster.h>
@@ -11,16 +13,41 @@
 #include <tclap/CmdLine.h>
 
 using SpikeRaster64 = raster::SpikeRaster64;
-using RasterQueue = moodycamel::ConcurrentQueue<SpikeRaster64>;
 using CmdLine = TCLAP::CmdLine;
 using StringArg = TCLAP::ValueArg<std::string>;
 using UShortArg = TCLAP::ValueArg<uint16_t>;
 using ArgException = TCLAP::ArgException;
 using ReceiverStatus = receiver::ReceiverStatus;
-using LinuxTCPReceiver = receiver::Receiver<LinuxTCPCore, RasterQueue>;
 using SafeThread = sthread::SaferThread;
 
-void receive(RasterQueue& q)
+template<typename T>
+class SharedQueue {
+public:
+  template<typename... Args>
+  void enqueue_bulk(Args&&... args) {
+    raster_queue_.enqueue_bulk(std::forward<Args>(args)...);
+    condition_.notify_one();
+  }
+
+  template<typename... Args>
+  bool wait_and_dequeue(Args&&... args) {
+    condition_.wait(lk, [this]{return this->raster_queue_.size_approx() > 0;});
+    auto result = raster_queue_.try_dequeue(std::forward<Args>(args)...);
+    return result;
+  }
+
+  bool size_approx() const {
+    return raster_queue_.size_approx() == 0;
+  }
+
+private:
+  moodycamel::ConcurrentQueue<T> raster_queue_;
+  std::condition_variable condition_;
+};
+
+using LinuxTCPReceiver = receiver::Receiver<LinuxTCPCore, SharedQueue<SpikeRaster64>>;
+
+void receive(SharedQueue<SpikeRaster64>& q)
 {
   LinuxTCPReceiver *receiver = new LinuxTCPReceiver(4096);
   if (receiver::ReceiverStatus::kOkay == receiver->get_status()) {
@@ -31,23 +58,16 @@ void receive(RasterQueue& q)
   }
 }
 
-void decode(RasterQueue& q)
+void decode(SharedQueue<SpikeRaster64>& q)
 {
   SpikeRaster64 found;
   size_t count = 0;
   while (true) {
-    while (!q.try_dequeue(found)) {
-    // bug in clang-tidy-12 spaceship operator parsing:
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    // workaround:
-      std::this_thread::sleep_until(
-        std::chrono::system_clock::now() +
-        std::chrono::microseconds(256)
-      );
-    }
-    std::cout << "# " << ++count << " ID: " << std::hex << found.id << std::endl;
-    for (auto r: found.raster) {
-      std::cout << "\t" << std::dec << r << std::endl;
+    if (q.wait_and_dequeue(found)) {
+      std::cout << "# " << ++count << " ID: " << std::hex << found.id << '\n';
+      for (auto r: found.raster) {
+        std::cout << "\t" << std::dec << r << '\n';
+      }
     }
   }
 }
@@ -71,20 +91,19 @@ int main(int argc, char *argv[])
     ip = arg_ip.getValue();
     port = arg_port.getValue();
 
-    std::cout << "Binding to " << ip << ": " << port << std::endl;
+    std::cout << "Binding to " << ip << ": " << port << '\n';
 
   } catch (ArgException &e) {
     std::cerr << "error: " << e.error() << " for arg " << e.argId()
-              << std::endl;
+              << '\n';
   }
 
-  auto raster_queue = RasterQueue();
+  auto raster_queue = SharedQueue<SpikeRaster64>();
   auto decodes = SafeThread(std::thread(decode, std::ref(raster_queue)), sthread::Action::kJoin);
   auto receives = SafeThread(std::thread(receive, std::ref(raster_queue)), sthread::Action::kJoin);
 
-  std::cout << "Now executing concurrently...\n" << std::endl;
-
+  std::cout << "Now executing concurrently...\n" << '\n';
   // Todo: write a logging thread instead of using stdout.
-
+  std::cout << std::flush;
   return 0;
 }
